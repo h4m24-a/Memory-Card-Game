ac89db2773087b5164dceb017230e3907da7c029
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-12-06 21:19:05 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl bg-red-300 rounded-xl font-bold text-center py-5'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
