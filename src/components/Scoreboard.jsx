4d39ee4f4fa673ebc8562c8156f171b53ba9fda2
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-12-30 21:42:15 +0000

    updated ui of scoreboard

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,17 +1,22 @@
 import PropTypes from "prop-types";
 
-
-const Scoreboard = ({score, bestScore}) => {
+const Scoreboard = ({ score, bestScore }) => {
   return (
-    <div className='flex flex-col justify-center items-center'>
-    <div>Score: {score} </div>
-    <div>Best Score: {bestScore} </div>
-    </div>
-  )
-}
+    <>
+      <div className="flex flex-row justify-center items-center gap-5 mt-5 mb-5">
+        <div className="font-poppins flex flex-row gap-4 bg-red-500 rounded-xl py-3 px-6">
+          <div>Score: {score} </div>
+        </div>
+        <div className="font-poppins flex flex-row gap-4 bg-blue-500 rounded-xl py-3 px-6"> 
+          <div>Best Score: {bestScore} </div>
+        </div>
+      </div>
+    </>
+  );
+};
 Scoreboard.propTypes = {
   score: PropTypes.number.isRequired,
   bestScore: PropTypes.number.isRequired,
 };
 
-export default Scoreboard
+export default Scoreboard;
