7373da447ba9b240876764a82d66d47d126cdf74
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-12-30 22:35:24 +0000

    updated spacings

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const Scoreboard = ({ score, bestScore }) => {
   return (
     <>
-      <div className="flex flex-row justify-center items-center gap-5 mt-5 mb-5">
+      <div className="flex flex-row justify-center items-center gap-5 mt-3 mb-5">
         <div className="font-poppins flex flex-row gap-4 bg-red-500 rounded-xl py-3 px-6">
           <div>Score: {score} </div>
         </div>
