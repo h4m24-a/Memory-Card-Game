0812970e3ace67af012d3c847cf9632b682a584e
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-01-02 16:41:25 +0000

    updated card ui

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,22 @@
 import PropTypes from "prop-types";
 
-const Card = ({ id, name, image, onClick}) => {
+const Card = ({ id, name, image, onClick }) => {
   return (
-    <div key={id} onClick={onClick} className="px-6 py-12 flex flex-col justify-center items-center rounded-lg shadow-xl cursor-pointer focus:bg-blue-500 focus:outline-none active:bg-blue-500 hover:shadow-lg hover:shadow-blue-500/50 lg:flex-shrink">
-      <img
-        src={image}
-        alt={name}
-        className="object-cover bg-yellow-300 rounded-xl object-center max-w-full rounded-t-ld h-40 md:40 lg:h-50"
-      />
-      <div className="flex flex-col mt-8">
-        <p className="text-2xl font-pokemonFont tracking-widest uppercase text-center text-zinc-900 ">{name}</p>
+    <div className="card w-60 bg-base-100 shadow-xl transition-transform transform hover:scale-105 ocus:bg-blue-500 focus:outline-none active:shadow-blue-500/50  lg:flex-shrink" key={id} onClick={onClick}>
+      <figure className="px-8 pt-14">
+        <img
+          src={image}
+          alt={name}
+          className="rounded-xl bg-yellow-200 object-cover max-w-full w-full object-center h-40 md:h-40 lg:h-50"
+        />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title text-2xl font-pokemonFont tracking-widest uppercase text-center text-zinc-900">{name}</h2>
       </div>
     </div>
   );
 };
+
 
 Card.propTypes = {
   id: PropTypes.number.isRequired,
